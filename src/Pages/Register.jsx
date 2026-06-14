587ff4aead5e88367679e5e7ca2e263999862196
Kishores2504 <kishore.s2504@gmail.com>
2026-06-14 22:40:30 +0530

    Register Login Form Altered

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -5,24 +5,31 @@ const Register = () => {
 
   return (
     <>
-      <div className="container" style={{ height: "100vh" }}>
+      <div className="container-md " style={{ height: "100vh" }}>
         <div className="row m-0 flex-md-row justify-content-center column-gap-3 align-items-center h-100">
           <div className="col d-none d-md-block">
             <h4 className="mb-2">Welcome to,</h4>
             <h1 className="display-1 fw-bold">Ai Powered Task Management</h1>
             <div className="mt-3 d-flex flex-column ">
-              <h4 className=" text-secondary-emphasis">Already Register ?</h4>
-              <button className="mt-2 align-self-start px-4 rounded-3 bg-transparent border-2" onClick={()=>{set_newUser((prev)=> !prev)}}>
+              <h4 className=" text-secondary-emphasis">
+                {newUser ? "Already Register ?" : "New User ?"}
+              </h4>
+              <button
+                className="mt-2 align-self-start px-4 rounded-3 bg-transparent border-2"
+                onClick={() => {
+                  set_newUser((prev) => !prev);
+                }}
+              >
                 {newUser ? "Login" : "Register"}
               </button>
             </div>
           </div>
-          <div className="col h-75 d-flex justify-content-center">
+          <div className=" col-md-5 col-sm-12 h-75 d-flex justify-content-center">
             <form
               action="post"
-              className="bg-transparent border border-3 p-3 h-100 w-75 d-flex flex-column justify-content-around align-items-center rounded-3"
+              className="bg-transparent border border-3 p-3 h-100 w-100 d-flex flex-column justify-content-around align-items-center rounded-3"
             >
-                <h1 className="d-md-none">Ai Powered Task Management </h1>
+              <h1 className="d-md-none">Ai Powered Task Management </h1>
               {newUser ? (
                 <>
                   <h1>Register Form</h1>
@@ -74,10 +81,21 @@ const Register = () => {
                       style={{ outline: "none" }}
                     />
                   </div>
-                  <div className="d-flex justify-content-around w-100">
-                    <button className="align-self-start btn btn-outline-dark">
-                      Register
-                    </button>
+                  <div className="d-flex flex-column row-gap-2 justify-content-around align-items-center w-100">
+                    <button className="btn btn-outline-dark">Register</button>
+                    <div className="d-sm-block d-md-none d-flex column-gap-2">
+                      <p className="mb-0 mt-2">
+                        {newUser ? "Already Register ?" : "New User ?"}{" "}
+                      </p>
+                      <button
+                        className=" align-self-start rounded-3 bg-transparent border-0"
+                        onClick={() => {
+                          set_newUser((prev) => !prev);
+                        }}
+                      >
+                        {newUser ? "Login" : "Register"}
+                      </button>
+                    </div>
                   </div>
                 </>
               ) : (
@@ -115,10 +133,21 @@ const Register = () => {
                       style={{ outline: "none" }}
                     />
                   </div>
-                  <div className="d-flex justify-content-around w-100">
-                    <button className="align-self-start btn btn-outline-dark">
-                      Login
-                    </button>
+                  <div className="d-flex flex-column row-gap-2 justify-content-around align-items-center w-100">
+                    <button className="btn btn-outline-dark">Login</button>
+                    <div className="d-sm-block d-md-none d-flex column-gap-2 align-items-center justif-content-center">
+                      <p className="mb-0 mt-2">
+                        {newUser ? "Already Register ?" : "New User ?"}{" "}
+                      </p>
+                      <button
+                        className=" align-self-start rounded-3 bg-transparent border-0"
+                        onClick={() => {
+                          set_newUser((prev) => !prev);
+                        }}
+                      >
+                        {newUser ? "Login" : "Register"}
+                      </button>
+                    </div>
                   </div>
                 </>
               )}
