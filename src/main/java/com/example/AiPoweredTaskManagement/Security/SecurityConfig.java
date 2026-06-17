d94aaffec2fe89c93dc0179ef05456645bc16c50
Kishores2504 <kishore.s2504@gmail.com>
2026-06-17 22:48:05 +0530

    security config modified

--- src/main/java/com/example/AiPoweredTaskManagement/Security/SecurityConfig.java
+++ src/main/java/com/example/AiPoweredTaskManagement/Security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsconfig() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:5173","https://ai-powered-task-manager-production.up.railway.app"));
+		config.setAllowedOrigins(List.of("http://localhost:5173","https://ai-powered-task-manager-one.vercel.app"));
 		config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
